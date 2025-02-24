0e4e475e0e7fd0310e451d8d4d3a110d5a4e0aa4
Björn Larsson <registrering@bjornlarsson.se>
2025-02-24 20:53:23 +0100

    Initial creation of the repo.

---
+++ ScreamCipher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ScreamCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
